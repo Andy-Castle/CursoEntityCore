98fa022e20e55c964fdd3ae38411a343057fcf37
Andy Castle <masterchief_and@live.com>
2025-01-07 13:40:24 -0500

    Add project files.

---
+++ CursoEntityCore/Datos/ApplicationDbContext.cs
@@ -0,0 +1,27 @@
+﻿using CursoEntityCore.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CursoEntityCore.Datos
+{
+    public class ApplicationDbContext : DbContext
+    {
+        //Esto es un constructor
+        //ctor --mas tab para crear el constructor
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> opciones) : base(opciones)
+        {
+        }
+
+        //Escribimos los modelos
+        public DbSet<Categoria> Categoria {  get; set; }
+
+
+
+
+        //Cuando crear migraciones (buenas prácticas)
+        //1 - Cuando se cree una nueva clase (tabla en la bd)
+        //2 - Cuando agregue una nueva propiedad (crear un campo nuevo en la bd)
+        //3 - Modifique un valor de campo en la clase (modificiar campo en bd) 
+
+        public DbSet<Usuario> Usuario { get; set; }
+    }
+}
---
+++ CursoEntityCore/Migrations/20250107181528_CambioNombreCategoriaPorInt.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CursoEntityCore.Migrations
+{
+    /// <inheritdoc />
+    public partial class CambioNombreCategoriaPorInt : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "Nombre",
+                table: "Categoria",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Nombre",
+                table: "Categoria",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int");
+        }
+    }
+}
