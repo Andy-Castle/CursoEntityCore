63a90ed8617f31221c5144f9194d6d25d48e3b42
Andy Castle <masterchief_and@live.com>
2025-01-09 14:19:10 -0500

    **Realización Tarea. Notas:** Aqui estuve viendo que para poder traer datos desde el servidor es necesario usar @Html.DisplayNameFor(c=>c.m) en cual nuestra tabla lo usara como cabecera. Tambien realizamos la vista y el controller de Artículos

---
+++ CursoEntityCore/Controllers/ArticulosController.cs
@@ -0,0 +1,25 @@
+﻿using CursoEntityCore.Datos;
+using CursoEntityCore.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CursoEntityCore.Controllers
+{
+    public class ArticulosController : Controller
+    {
+
+        public readonly ApplicationDbContext _context;
+
+        public ArticulosController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+
+        public IActionResult Index()
+        {
+            List<Articulo> listaArticulos = _context.Articulo.ToList();
+
+            return View(listaArticulos);
+        }
+    }
+}
