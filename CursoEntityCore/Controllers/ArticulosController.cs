77dbac750520021c5f1baf18ef73f3acf5b1464b
Andy Castle <masterchief_and@live.com>
2025-01-09 15:39:10 -0500

    **Realización de Tarea. Notas:** Aqui coloque la forma de crear un select que me traiga el Categoria_Id y los Nombres de este, y tambien otro problema que cuando el controller de get intentaba traer los datos de Articulo, los datos relacionados los traia como null, digamos que el metodo get los traia tan rapido que no daba tiempo de verificar que estos llevaban estos datos y esto se soluciono con un .Include(articulo => articulo.Categoria) y tambien se añadie lo vista de crear y el ViewMovel de ArticuloCategoriaVM

--- CursoEntityCore/Controllers/ArticulosController.cs
+++ CursoEntityCore/Controllers/ArticulosController.cs
@@ -1,6 +1,8 @@
 ﻿using CursoEntityCore.Datos;
 using CursoEntityCore.Models;
+using CursoEntityCore.ViewModels;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace CursoEntityCore.Controllers
 {
@@ -14,12 +16,45 @@ namespace CursoEntityCore.Controllers
             _context = context;
         }
 
-
+        [HttpGet]
         public IActionResult Index()
         {
-            List<Articulo> listaArticulos = _context.Articulo.ToList();
+            //Carga Ansiosa, debemos especificar que se deben de cargar entidades relacionadas con .Include
+            List<Articulo> listaArticulos = _context.Articulo.Include(articulo => articulo.Categoria).ToList();
 
             return View(listaArticulos);
+        }
+
+        [HttpGet]
+        public IActionResult Crear()
+        {
+            //Aqui creamos la instancia
+            ArticuloCategoriaVM articuloCategorias = new ArticuloCategoriaVM();
+
+            //Aqui de articulosCategorias seleccionamos el Nombre de la cateogira y su Id lo pasamos a string
+            articuloCategorias.ListaCategorias = _context.Categoria.Select(i => new Microsoft.AspNetCore.Mvc.Rendering.SelectListItem
+            {
+                Text = i.Nombre,
+                Value = i.Categoria_Id.ToString()
+            });
+
+
+            return View(articuloCategorias);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Crear(Articulo articulo)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Articulo.Add(articulo);
+                _context.SaveChanges();
+                return RedirectToAction(nameof(Index));
+                
+            }
+
+            return View();
         }
     }
 }
---
+++ CursoEntityCore/ViewModels/ArticuloCategoriaVM.cs
@@ -0,0 +1,15 @@
+﻿using CursoEntityCore.Models;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace CursoEntityCore.ViewModels
+{
+    /*
+     Aqui se creo una clase en la cual queremos obtener las Categorias_Id
+    y el Nombre de estas para poder mostrarlas en el front
+     */
+    public class ArticuloCategoriaVM
+    {
+        public Articulo Articulo { get; set; }
+        public IEnumerable<SelectListItem> ListaCategorias { get; set; }
+    }
+}
