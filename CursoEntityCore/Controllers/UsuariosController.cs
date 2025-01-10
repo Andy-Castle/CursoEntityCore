bdc28fed7c9c745d456fbbc2faac0b469dd4133a
Andy Castle <masterchief_and@live.com>
2025-01-10 15:10:51 -0500

    **Tarea UsuarioDetalle. Notas:** Se creo el apartado de editar del Usuario

--- CursoEntityCore/Controllers/UsuariosController.cs
+++ CursoEntityCore/Controllers/UsuariosController.cs
@@ -40,63 +40,6 @@ namespace CursoEntityCore.Controllers
             return View();
         }
 
-        [HttpGet]
-        public IActionResult CrearMultipleOpcion2()
-        {
-            List<Categoria> categorias = new List<Categoria>();
-            for (int i = 0; i < 2; i++)
-            {
-                categorias.Add(new Categoria { Nombre = Guid.NewGuid().ToString() });
-                //_context.Categoria.Add(new Categoria { Nombre = Guid.NewGuid().ToString() });
-            }
-            _context.Categoria.AddRange(categorias);
-            _context.SaveChanges();
-            return RedirectToAction(nameof(Index));
-        }
-
-        [HttpGet]
-        public IActionResult CrearMultipleOpcion5()
-        {
-            List<Categoria> categorias = new List<Categoria>();
-            for (int i = 0; i < 5; i++)
-            {
-                categorias.Add(new Categoria { Nombre = Guid.NewGuid().ToString() });
-                //_context.Categoria.Add(new Categoria { Nombre = Guid.NewGuid().ToString() });
-            }
-
-            _context.Categoria.AddRange(categorias);
-            _context.SaveChanges();
-            return RedirectToAction(nameof(Index));
-        }
-
-        [HttpGet]
-
-        public IActionResult VistaCrearMultipleOpcionFormulario()
-        {
-            return View();
-        }
-
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public IActionResult CrearMultipleOpcionFormulario()
-        {
-            string categoriasForm = Request.Form["Nombre"];
-            var listaCategorias = from val in categoriasForm.Split(new[] {","}, StringSplitOptions.RemoveEmptyEntries) select (val);
-
-            List<Categoria> categorias = new List<Categoria>();
-
-            foreach (var categoria in listaCategorias)
-            {
-                categorias.Add(new Categoria 
-                {
-                    Nombre = categoria
-                });
-            }
-
-            _context.Categoria.AddRange(categorias);
-            _context.SaveChanges();
-            return RedirectToAction(nameof(Index));
-        }
 
         [HttpGet]
         public IActionResult Editar (int? id)
@@ -106,23 +49,23 @@ namespace CursoEntityCore.Controllers
                 return View();
             }
 
-            var categoria = _context.Categoria.FirstOrDefault(categoria => categoria.Categoria_Id == id);
-            return View(categoria);
+            var usuario = _context.Usuario.FirstOrDefault(usuario => usuario.Id == id);
+            return View(usuario);
         }
 
 
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public IActionResult Editar(Categoria categoria)
+        public IActionResult Editar(Usuario usuario)
         {
             if (ModelState.IsValid)
             {
-                _context.Categoria.Update(categoria);
+                _context.Usuario.Update(usuario);
                 _context.SaveChanges();
                 return RedirectToAction(nameof(Index));
             }
 
-            return View(categoria);
+            return View(usuario);
         }
 
         [HttpGet]
@@ -134,22 +77,6 @@ namespace CursoEntityCore.Controllers
             return RedirectToAction(nameof(Index));
         }
 
-        [HttpGet]
-        public IActionResult BorrarMultiple2()
-        {
-            IEnumerable<Categoria> categorias = _context.Categoria.OrderByDescending(categoria => categoria.Categoria_Id).Take(2);
-            _context.Categoria.RemoveRange(categorias);
-            _context.SaveChanges();
-            return RedirectToAction(nameof(Index));
-        }
-
-        [HttpGet]
-        public IActionResult BorrarMultiple5()
-        {
-            IEnumerable<Categoria> categorias = _context.Categoria.OrderByDescending(categoria => categoria.Categoria_Id).Take(5);
-            _context.Categoria.RemoveRange(categorias);
-            _context.SaveChanges();
-            return RedirectToAction(nameof(Index));
-        }
+        
     }
 }
